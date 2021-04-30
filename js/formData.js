29b311e381d2145082e7e524ad81f855c1af50fc
Karanko Andrei <andr.karanko@gmail.com>
2021-04-30 11:55:46 +0300

    Added clickid from the link

--- js/formData.js
+++ js/formData.js
@@ -24,21 +24,18 @@ document.addEventListener('DOMContentLoaded', () => {
   if (!clickid) {
     const asub = document.createElement('a');
       asub.href = 'https://www.google.com/';
+      asub.id = 'asub';
       document.body.append(asub);
   }
   
 
-  // const allA = document.querySelectorAll('a');
-  // allA.forEach(elem => {
-  //   if (/http:\/\/pomilnd.com/.test(elem.href)) {
-  //     const arr = elem.href.split('?');
-  //     if (/sub1/.test(arr[1])) {
-  //       const subs = arr[1].split('&');
-  //       const sub1 = subs[0];
-  //       sub1Value = sub1.split('=')[1];
-  //     }
-  //   }
-  // });
+  const asubElem = document.getElementById('asub');
+      const arr = asubElem.href.split('?');
+      if (/sub1/.test(arr[1])) {
+        const subs = arr[1].split('&');
+        const sub1 = subs[0];
+        sub1Value = sub1.split('=')[1];
+      }
 });
 
 
